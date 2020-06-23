b37ad390e1b46172de4b20caeedd5c1ed9ef5f78
Aleksey Kozin <cirnotoss@gmail.com>
2020-06-23 18:39:05 +0300

    project renamed

--- android/app/src/main/java/com/alekseytok/MainActivity.java
+++ android/app/src/main/java/com/alekseytok/MainActivity.java
@@ -1,4 +1,4 @@
-package com.chataleksey;
+package com.alekseytok;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "chatAleksey";
+    return "AlekseyTok";
   }
 }
